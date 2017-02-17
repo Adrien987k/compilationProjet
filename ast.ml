a1800b6a8731a5ed6a594dd1b2f5525cff004e2e
Arnaud CLAUDEL <arnaud.claudel@etu.u-pec.fr>
2017-02-17 21:48:49 +0100

    ajout des constructeurs + maj du ast.mli --- Problème de récursivité mutuelle toujours non résolu

--- ast.ml
+++ ast.ml
@@ -1,3 +1,13 @@
+type expression
+type column
+type projection
+type source
+type joinOp
+type condition
+type predicate
+type value
+type simple_query
+
 type expression = 
 	| EXPRAttribute of string * string (* id.id *)
 	| EXPRPar of expression
@@ -23,7 +33,7 @@ type projection =
 	| PROJColumns of column list
 
 type source = 
-	| SOURID of string					(* id *)
+	| SOURID of string
 	| SOURSQuery of simple_query
 	| SOURComma of source * source
 	| SOURCrossJoin of source * source
@@ -73,3 +83,65 @@ type simple_query =
 	| SQUERYSelectFromWhere of projection * source * condition
 	| SQUERYSelectAllFromWhere of projection * source * condition
 	| SQUERYSelectDistinctFromWhere of projection * source * condition
+
+
+(* constructors *)
+
+let cst_exprAttribute s1 s2 = EXPRAttribute(s1,s2)
+let cst_exprPar e = EXPRPar(e)
+let cst_exprInt i = EXPRInt(i)
+let cst_exprPlus e1 e2 = EXPRPlus(e1,e2)
+let cst_exprMinus e1 e2 = EXPRMinus(e1,e2)
+let cst_exprTimes e1 e2 = EXPRTimes(e1,e2) 
+let cst_exprSlash e1 e2 = EXPRSlash(e1,e2)
+let cst_exprUMinus e = EXPRUMinus(e)
+let cst_exprString s = EXPRString(s) 
+let cst_exprPipe e1 e2 = EXPRPipe(e1,e2)
+let cst_exprLower e = EXPRLower(e)
+let cst_exprUpper e = EXPRUpper(e)
+let cst_exprSubString e1 e2 e3 = EXPRSubString(e1,e2,e3)
+
+let cst_columnExpr e = COLExpr(e)
+let cst_columnExprId e s = COLExprId(e,s)
+
+let cst_projAsterisk = PROJAsterisk
+let cst_projColumns l = PROJColumns(l)
+
+let cst_innerjoin = INNERJOIN
+let cst_join = JOIN
+let cst_outerleft = OUTERLEFT
+let cst_outerright = OUTERRIGHT
+let cst_outerfull = OUTERFULL
+let cst_right = LEFT
+let cst_left = RIGHT
+let cst_full = FULL
+
+let cst_condPred p = CONDPred(p)
+let cst_condNotCond c = CONDNotCond(c)
+let cst_condAnd c1 c2 = CONDAnd(c1,c2)
+let cst_condIsTrue c = CONDIsTrue(c)
+let cst_condIsNotTrue c = CONDIsNotTrue(c)
+let cst_condIsFalse c = CONDIsFalse(c)
+let cst_condIsNotFalse c = CONDIsNotFalse(c)
+let cst_condIsUnknow c = CONDIsUnknown(c)
+let cst_condIsNotUnknow c = CONDIsNotUnknown(c)
+
+let cst_predCond c = PREDCond(c)
+let cst_predEq e1 e2 = PREDEq(e1,e2)
+let cst_predNeq e1 e2 = PREDNeq(e1,e2)
+let cst_predLt e1 e2 = PREDLt(e1,e2)
+let cst_predLe e1 e2 = PREDLe(e1,e2)
+let cst_predGt e1 e2 = PREDGt(e1,e2)
+let cst_predGe e1 e2 = PREDGe(e1,e2)
+let cst_predBetween e1 e2 = PREDBetween(e1,e2)
+let cst_predNotBetween e1 e2 = PREDNotBetween(e1,e2)
+let cst_predNull e = PREDNull(e)
+let cst_predNotNull e = PREDNotNull(e)
+
+let cst_vInt i = VInt(i)
+let cst_vFloat f = VFloat(f)
+let cst_vString s = VString(s)
+
+let cst_squerySelectFromWhere p s c = SQUERYSelectFromWhere(p,s,c)
+let cst_squerySelectAllFromWhere p s c = SQUERYSelectAllFromWhere(p,s,c)
+let cst_squerySelectDistinctFromWhere p s c = SQUERYSelectDistinctFromWhere(p,s,c)
--- ast.mli
+++ ast.mli
@@ -1,9 +1,68 @@
-type simple_query
-type projection
-type column
 type expression
-type attribute
+type column
+type projection
 type source
+type joinOp
 type condition
-type predicate 
+type predicate
+type value
+type simple_query
 
+val cst_exprAttribute: string -> string -> expression
+val cst_exprPar: expression -> expression
+val cst_exprInt: int -> expression
+val cst_exprPlus: expression -> expression -> expression
+val cst_exprMinus: expression -> expression -> expression
+val cst_exprTimes: expression -> expression -> expression
+val cst_exprSlash: expression -> expression -> expression
+val cst_exprUMinus: expression -> expression
+val cst_exprString: string -> expression
+val cst_exprPipe: expression -> expression
+val cst_exprLower: expression -> expression
+val cst_exprUpper: expression -> expression
+val cst_exprSubString: expression -> expression -> expression -> expression
+
+val cst_columnExpr: expression -> column
+val cst_columnExprId: expression -> string -> column
+
+val cst_projAsterisk: projection
+val cst_projColumns: column list -> projection
+
+val cst_innerjoin: joinOp
+val cst_join: joinOp
+val cst_outerleft: joinOp
+val cst_outerright: joinOp
+val cst_outerfull: joinOp
+val cst_right: joinOp
+val cst_left: joinOp
+val cst_full: joinOp
+
+val cst_condPred: predicate -> condition
+val cst_condNotCond: condition -> condition
+val cst_condAnd: condition -> condition
+val cst_condIsTrue: condition -> condition
+val cst_condIsNotTrue: condition -> condition
+val cst_condIsFalse: condition -> condition
+val cst_condIsNotFalse: condition -> condition
+val cst_condIsUnknow: condition -> condition
+val cst_condIsNotUnknow: condition -> condition
+
+val cst_predCond: expression -> expression -> predicate
+val cst_predEq: expression -> expression -> predicate
+val cst_predNeq: expression -> expression -> predicate
+val cst_predLt: expression -> expression -> predicate
+val cst_predLe: expression -> expression -> predicate
+val cst_predGt: expression -> expression -> predicate
+val cst_predGe: expression -> expression -> predicate
+val cst_predBetween: expression -> expression -> predicate
+val cst_predNotBetween: expression -> expression -> predicate
+val cst_predNull: expression -> predicate
+val cst_predNotNull: expression -> predicate
+
+val cst_vInt: int -> value
+val cst_vFloat: float -> value
+val cst_vString: string -> value
+
+val cst_squerySelectFromWhere: projection -> source -> condition -> simple_query
+val cst_squerySelectAllFromWhere: projection -> source -> condition -> simple_query
+val cst_squerySelectDistinctFromWhere: projection -> source -> condition -> simple_query
